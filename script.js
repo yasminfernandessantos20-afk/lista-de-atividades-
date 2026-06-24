45ca19e10b1707e9b7b8554acc1bf1395e0fdd12
yasminfernandessantos20-afk <yasmin.fernandes.santos20@escola.pr.gov.br>
2026-06-24 12:31:25 -0300

    Criação de conexão om arquivo local JSON

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
 const form = document.getElementById('form')
 const input = document.getElementsByClassName('input')
 const todoUL = document.getElementById('todo')
+
+const todos = JSON.parse(localStorage.getItem('todos'))
