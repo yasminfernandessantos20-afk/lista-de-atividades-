c29e5a8082885ad5cf6f091f2fc83db84cb65f3b
yasminfernandessantos20-afk <yasmin.fernandes.santos20@escola.pr.gov.br>
2026-06-24 12:35:26 -0300

    if todos

--- script.js
+++ script.js
@@ -3,3 +3,6 @@ const input = document.getElementsByClassName('input')
 const todoUL = document.getElementById('todo')
 
 const todos = JSON.parse(localStorage.getItem('todos'))
+
+if(todos){
+    todos.forEach(todo => addTodo(todo))}
